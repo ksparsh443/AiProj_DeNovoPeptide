16af5f284ddca961997f2e339e5f0fae8c402986
Sparsh kandpal <ksparsh443@gmail.com>
2024-10-26 12:37:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ import biotite.structure.io as bsio
 
 # Set page config and sidebar title
 st.set_page_config(layout='wide')
-st.sidebar.title('🎈 ESMFold')
-st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
+st.sidebar.title('DE NOVO Visualise')
 
 # Function to render protein structure
 def render_mol(pdb):
